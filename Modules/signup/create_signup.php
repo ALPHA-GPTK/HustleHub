8d3fe74cbcb55917b19af16f65dc6e3db598b58d
PrynsTag <201812311@fit.edu.ph>
2021-04-02 02:48:11 +0800

    Added confirm passwords and security such as hashing and prepared statement

--- Modules/signup/create_signup.php
+++ Modules/signup/create_signup.php
@@ -4,20 +4,23 @@ if (isset($_POST["signup"])) {
     $fullName = sprintf("%s %s", $_POST["fName"], $_POST["lName"]);
     $userName = $_POST["username"];
     $email = $_POST["email"];
-    $password = $_POST["password"];
+    $password = sha1($_POST["password"]);
 
     if (isset($conn) & $conn) {
-        $query = "INSERT INTO `freelance_db`.freelance_info (freelance_fullname, freelance_username, freelance_email, freelance_password) 
-                  VALUES ('$fullName', '$userName', '$email', '$password');";
-        $mysqli = mysqli_query($conn, $query);
+        $sql = "INSERT INTO `freelance_db`.freelance_info (freelance_fullname, freelance_username, freelance_email, freelance_password) 
+                  VALUES (?, ?, ?, ?);";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "ssss", $fullName, $userName, $email, $password);
+        $query = mysqli_stmt_execute($stmt);
 
-        if ($mysqli !== false) {
+        if ($query !== false) {
             header("Location: ../login/login.php?signup=success");
         } else {
             echo "<script>alert('Signup Failed')</script>";
             header("Location: ./signup.php?signup=fail");
         }
+        $conn->close();
     } else {
-        die("Connection Failed: " . mysqli_error($conn));
+        die("Connection Failed: " . $conn->erorr());
     }
 }
--- Modules/signup/signup.php
+++ Modules/signup/signup.php
@@ -1,7 +1,8 @@
 <?php require_once "../../templates/header.php" ?>
-  <div class="flex items-center h-screen bg-blue-50 dark:bg-gray-900 overflow-hidden">
-    <div class="container mx-auto">
-      <form action="./create_signup.php" method="post">
+  <div class="flex items-center h-screen bg-blue-50 dark:bg-gray-900">
+    <div class="container mx-auto overflow-hidden">
+      <form action="./create_signup.php" method="post"
+            oninput='password.setCustomValidity(password.value !== confirm_password.value ? "Password is not the same with the confirm password." : "")'>
         <div class="max-w-lg flex flex-wrap mx-auto md:space-y-4">
           <h1 class="text-3xl my-4 mx-auto text-blue dark:text-gray-200 uppercase font-black">Create Account</h1>
           <!-- First Name -->
@@ -10,7 +11,7 @@
               First name
             </label>
             <input class="w-full h-10 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                   type="text" name="fName" id="fName"/>
+                   type="text" name="fName" id="fName" required/>
           </div>
           <!-- Last Name -->
           <div class="w-full pl-2 md:w-1/2">
@@ -28,7 +29,7 @@
             <div class="relative text-gray-700">
               <label>
                 <input class="w-full h-10 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                       type="text" name="username" id="username"/>
+                       type="text" name="username" id="username" required/>
               </label>
               <div class="absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none">
                 <svg class="h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -45,7 +46,7 @@
             <div class="relative text-gray-700">
               <label>
                 <input class="w-full h-10 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                       type="email" name="email" id="email"/>
+                       type="email" name="email" id="email" required/>
               </label>
               <div class="absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none">
                 <svg class="h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -62,7 +63,7 @@
             <div class="relative text-gray-700">
               <label>
                 <input class="w-full h-10 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                       type="password" name="password" id="password"/>
+                       type="password" name="password" id="password" required/>
               </label>
               <div class="absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none">
                 <svg class="h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -70,6 +71,24 @@
                   <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"/>
                   <path fill-rule="evenodd"
                         d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                        clip-rule="evenodd"/>
+                </svg>
+              </div>
+            </div>
+            <!-- Confirm Password -->
+            <label for="confirm_password" class="block text-xs text-gray-600 dark:text-gray-400 uppercase font-black">
+              confirm password
+            </label>
+            <div class="relative text-gray-700">
+              <label>
+                <input class="w-full h-10 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                       type="password" name="confirm_password" id="confirm_password" required/>
+              </label>
+              <div class="absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none">
+                <svg class="h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                     fill="currentColor">
+                  <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                         clip-rule="evenodd"/>
                 </svg>
               </div>
