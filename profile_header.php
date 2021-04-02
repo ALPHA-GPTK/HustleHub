f02a3f5202e3f92f94db577333d1b937176fbcc4
Tricia Relox <55880592+3ciadgr@users.noreply.github.com>
2021-04-02 20:15:50 +0800

    change profile.php logout location

--- profile_header.php
+++ profile_header.php
@@ -61,7 +61,7 @@
         <!-- Mobile menu, show/hide based on menu state. -->
         <div class="sm:hidden" id="mobile-menu">
             <div class="px-2 pt-2 pb-3 space-y-1">
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">LOG OUT</a>
+                <a href="logout.php" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">LOG OUT</a>
             </div>
         </div>
     </nav>
