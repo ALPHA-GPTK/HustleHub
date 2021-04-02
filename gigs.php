a845def79b88a980b9bcfee3c65b81f97ab882c2
Kathorsiii <55967594+Kathorsiii@users.noreply.github.com>
2021-04-02 16:30:42 +0800

    Update gigs.php

--- gigs.php
+++ gigs.php
@@ -1,3 +1,134 @@
 <?php require_once "header.php" ?>
 
+<section class="provider mx-auto">
+    <div>
+        <h1 class="title-header font-bold">MyGigs</h1>
+    </div>
+
+    <div class="antialiased pl-14 pr-14">
+        <div class="flex flex-wrap -mx-4">
+            <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                <a href="" class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                    <div class="relative pb-48 overflow-hidden">
+                        <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+                    </div>
+                    <div class="p-4">
+                        <div class="px-6 py-4">
+                            <span class="text-gray-700 float-right">
+                                3 days ago
+                            </span>
+                            <div class="font-bold text-xl mb-2 mt-10">Programming</div>
+                            <span class="text-gray-700">
+                                PRICE
+                            </span>
+                            <span class="font-extrabold">
+                                P5,000
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+                        <span class="pl-2">
+                            <img src="assets/img/stars.svg" alt="">
+                        </span>
+                    </div>
+                    <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid px-6 pt-4 pb-2">
+                        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border m-3 border-blue-500 hover:border-transparent rounded-lg">UPDATE</button>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">DELETE</button>
+                    </div>
+                </a>
+            </div>
+            <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                <a href="" class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                    <div class="relative pb-48 overflow-hidden">
+                        <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+                    </div>
+                    <div class="p-4">
+                        <div class="px-6 py-4">
+                            <span class="text-gray-700 float-right">
+                                3 days ago
+                            </span>
+                            <div class="font-bold text-xl mb-2 mt-10">Programming</div>
+                            <span class="text-gray-700">
+                                PRICE
+                            </span>
+                            <span class="font-extrabold">
+                                P5,000
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+                        <span class="pl-2">
+                            <img src="assets/img/stars.svg" alt="">
+                        </span>
+                    </div>
+                    <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid px-6 pt-4 pb-2">
+                        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border m-3 border-blue-500 hover:border-transparent rounded-lg">UPDATE</button>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">DELETE</button>
+                    </div>
+                </a>
+            </div>
+            <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                <a href="" class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                    <div class="relative pb-48 overflow-hidden">
+                        <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+                    </div>
+                    <div class="p-4">
+                        <div class="px-6 py-4">
+                            <span class="text-gray-700 float-right">
+                                3 days ago
+                            </span>
+                            <div class="font-bold text-xl mb-2 mt-10">Programming</div>
+                            <span class="text-gray-700">
+                                PRICE
+                            </span>
+                            <span class="font-extrabold">
+                                P5,000
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+                        <span class="pl-2">
+                            <img src="assets/img/stars.svg" alt="">
+                        </span>
+                    </div>
+                    <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid px-6 pt-4 pb-2">
+                        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border m-3 border-blue-500 hover:border-transparent rounded-lg">UPDATE</button>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">DELETE</button>
+                    </div>
+                </a>
+            </div>
+            <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                <a href="" class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                    <div class="relative pb-48 overflow-hidden">
+                        <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+                    </div>
+                    <div class="p-4">
+                        <div class="px-6 py-4">
+                            <span class="text-gray-700 float-right">
+                                3 days ago
+                            </span>
+                            <div class="font-bold text-xl mb-2 mt-10">Programming</div>
+                            <span class="text-gray-700">
+                                PRICE
+                            </span>
+                            <span class="font-extrabold">
+                                P5,000
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+                        <span class="pl-2">
+                            <img src="assets/img/stars.svg" alt="">
+                        </span>
+                    </div>
+                    <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid px-6 pt-4 pb-2">
+                        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border m-3 border-blue-500 hover:border-transparent rounded-lg">UPDATE</button>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">DELETE</button>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php require_once "footer.php" ?>
