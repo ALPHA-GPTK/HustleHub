55a2f0ed225a784a1b6ed08507902777e394207b
Tricia Relox <55880592+3ciadgr@users.noreply.github.com>
2021-04-03 16:20:25 +0800

    added favicon

--- mygigs.php
+++ mygigs.php
@@ -1,162 +1,164 @@
-<div class="bg-lightBlue_8">
-    <?php require_once "./profile_header.php"; ?>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" s href="assets/img/favicon.png">
+    <div class="bg-lightBlue_8">
+        <?php require_once "./profile_header.php"; ?>
 
-    <section class="mx-auto">
-        <div>
-            <h1 class="title-header font-bold">MyGigs</h1>
-        </div>
+        <section class="mx-auto">
+            <div>
+                <h1 class="title-header font-bold">MyGigs</h1>
+            </div>
 
-        <div class="antialiased pl-14 pr-14">
-            <div class="flex flex-wrap -mx-4">
-                <!-- Card 1 -->
-                <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
-                    <div href="" class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-                        <div class="relative pb-48 overflow-hidden">
-                            <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
-                        </div>
-                        <div class="p-4">
-                            <div class="px-6 py-4">
-                                <span class="text-gray-700 float-right">
-                                    3 days ago
-                                </span>
-                                <div class="font-bold text-xl mb-2 mt-10">Programming</div>
-                                <span class="text-gray-700">
-                                    PRICE
-                                </span>
-                                <span class="font-extrabold">
-                                    P5,000
+            <div class="antialiased pl-14 pr-14">
+                <div class="flex flex-wrap -mx-4">
+                    <!-- Card 1 -->
+                    <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                        <div href="" class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                            <div class="relative pb-48 overflow-hidden">
+                                <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+                            </div>
+                            <div class="p-4">
+                                <div class="px-6 py-4">
+                                    <span class="text-gray-700 float-right">
+                                        3 days ago
+                                    </span>
+                                    <div class="font-bold text-xl mb-2 mt-10">Programming</div>
+                                    <span class="text-gray-700">
+                                        PRICE
+                                    </span>
+                                    <span class="font-extrabold">
+                                        P5,000
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+                                <span class="pl-2">
+                                    <img src="assets/img/stars.svg" alt="">
                                 </span>
                             </div>
-                        </div>
-                        <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
-                            <span class="pl-2">
-                                <img src="assets/img/stars.svg" alt="">
-                            </span>
-                        </div>
-                        <!-- Buttons -->
-                        <div class="grid grid-cols-2 p-10">
-                            <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
-                                <button class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</button>
-                            </div>
-                            <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
-                                <button class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</button>
+                            <!-- Buttons -->
+                            <div class="grid grid-cols-2 p-10">
+                                <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
+                                    <button class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</button>
+                                </div>
+                                <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
+                                    <button class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <!-- Card 2 -->
-                <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
-                    <div class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-                        <div class="relative pb-48 overflow-hidden">
-                            <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
-                        </div>
-                        <div class="p-4">
-                            <div class="px-6 py-4">
-                                <span class="text-gray-700 float-right">
-                                    3 days ago
-                                </span>
-                                <div class="font-bold text-xl mb-2 mt-10">Programming</div>
-                                <span class="text-gray-700">
-                                    PRICE
-                                </span>
-                                <span class="font-extrabold">
-                                    P5,000
+                    <!-- Card 2 -->
+                    <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                        <div class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                            <div class="relative pb-48 overflow-hidden">
+                                <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+                            </div>
+                            <div class="p-4">
+                                <div class="px-6 py-4">
+                                    <span class="text-gray-700 float-right">
+                                        3 days ago
+                                    </span>
+                                    <div class="font-bold text-xl mb-2 mt-10">Programming</div>
+                                    <span class="text-gray-700">
+                                        PRICE
+                                    </span>
+                                    <span class="font-extrabold">
+                                        P5,000
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+                                <span class="pl-2">
+                                    <img src="assets/img/stars.svg" alt="">
                                 </span>
                             </div>
-                        </div>
-                        <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
-                            <span class="pl-2">
-                                <img src="assets/img/stars.svg" alt="">
-                            </span>
-                        </div>
-                        <!-- Buttons -->
-                        <div class="grid grid-cols-2 p-10">
-                            <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
-                                <button class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</button>
-                            </div>
-                            <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
-                                <button class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</button>
+                            <!-- Buttons -->
+                            <div class="grid grid-cols-2 p-10">
+                                <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
+                                    <button class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</button>
+                                </div>
+                                <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
+                                    <button class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <!-- Card 3 -->
-                <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
-                    <div class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-                        <div class="relative pb-48 overflow-hidden">
-                            <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
-                        </div>
-                        <div class="p-4">
-                            <div class="px-6 py-4">
-                                <span class="text-gray-700 float-right">
-                                    3 days ago
-                                </span>
-                                <div class="font-bold text-xl mb-2 mt-10">Programming</div>
-                                <span class="text-gray-700">
-                                    PRICE
-                                </span>
-                                <span class="font-extrabold">
-                                    P5,000
+                    <!-- Card 3 -->
+                    <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                        <div class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                            <div class="relative pb-48 overflow-hidden">
+                                <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+                            </div>
+                            <div class="p-4">
+                                <div class="px-6 py-4">
+                                    <span class="text-gray-700 float-right">
+                                        3 days ago
+                                    </span>
+                                    <div class="font-bold text-xl mb-2 mt-10">Programming</div>
+                                    <span class="text-gray-700">
+                                        PRICE
+                                    </span>
+                                    <span class="font-extrabold">
+                                        P5,000
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+                                <span class="pl-2">
+                                    <img src="assets/img/stars.svg" alt="">
                                 </span>
                             </div>
-                        </div>
-                        <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
-                            <span class="pl-2">
-                                <img src="assets/img/stars.svg" alt="">
-                            </span>
-                        </div>
-                        <!-- Buttons -->
-                        <div class="grid grid-cols-2 p-10">
-                            <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
-                                <button class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</button>
-                            </div>
-                            <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
-                                <button class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</button>
+                            <!-- Buttons -->
+                            <div class="grid grid-cols-2 p-10">
+                                <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
+                                    <button class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</button>
+                                </div>
+                                <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
+                                    <button class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <!-- Card 4 -->
-                <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
-                    <div class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-                        <div class="relative pb-48 overflow-hidden">
-                            <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
-                        </div>
-                        <div class="p-4">
-                            <div class="px-6 py-4">
-                                <span class="text-gray-700 float-right">
-                                    3 days ago
-                                </span>
-                                <div class="font-bold text-xl mb-2 mt-10">Programming</div>
-                                <span class="text-gray-700">
-                                    PRICE
-                                </span>
-                                <span class="font-extrabold">
-                                    P5,000
+                    <!-- Card 4 -->
+                    <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                        <div class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                            <div class="relative pb-48 overflow-hidden">
+                                <img class="absolute inset-0 h-full w-full object-cover" src="https://images.pexels.com/photos/1181216/pexels-photo-1181216.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+                            </div>
+                            <div class="p-4">
+                                <div class="px-6 py-4">
+                                    <span class="text-gray-700 float-right">
+                                        3 days ago
+                                    </span>
+                                    <div class="font-bold text-xl mb-2 mt-10">Programming</div>
+                                    <span class="text-gray-700">
+                                        PRICE
+                                    </span>
+                                    <span class="font-extrabold">
+                                        P5,000
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+                                <span class="pl-2">
+                                    <img src="assets/img/stars.svg" alt="">
                                 </span>
                             </div>
-                        </div>
-                        <div class="flex leading-none justify-center items-center w-full divide-x divide-gray-400 divide-solid">
-                            <span class="pl-2">
-                                <img src="assets/img/stars.svg" alt="">
-                            </span>
-                        </div>
-                        <!-- Buttons -->
-                        <div class="grid grid-cols-2 p-10">
-                            <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
-                                <button class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</button>
-                            </div>
-                            <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
-                                <button class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</button>
+                            <!-- Buttons -->
+                            <div class="grid grid-cols-2 p-10">
+                                <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
+                                    <button class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</button>
+                                </div>
+                                <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
+                                    <button class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
-<?php require_once "footer.php" ?>
+        </section>
+    </div>
+    <?php require_once "footer.php" ?>
