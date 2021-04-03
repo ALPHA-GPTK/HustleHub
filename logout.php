10d6eed0b36233867593d8a91f5fc7262cf68d98
PrynsTag <201812311@fit.edu.ph>
2021-04-03 13:40:01 +0800

    Add logout functionality

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 if (isset($_SESSION["user_email"])) {
-    session_destroy(); 
-    header("Location: login.php?logout=successful");
+    session_destroy();
+    echo "<script>alert('Logout Successfully.')
+    window.location.href='login.php?account_status=logout_successful';
+    </script>";
 }
