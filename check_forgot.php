ae3a7e4d6a79ed87e66e9d85216a133290163f10
Tricia Relox <55880592+3ciadgr@users.noreply.github.com>
2021-04-03 17:11:01 +0800

    changed email format

--- check_forgot.php
+++ check_forgot.php
@@ -22,7 +22,9 @@ if (isset($_POST['email'], $conn, $mail, $gmailUsername)) {
 
             $mail->Subject = "Reset Password - HustleHub";
             $mail->Body = "<h1 class=\"h1-gmail\">Verification Code</h1><br/>
-                            <h3>Please copy this code</h3><br/>
+                            <h3>This email is to confirm a recent password reset request for your 
+                            HustleHub account.</h3><br/>
+                            <h3>To confirm this request and reset your password, please copy the code:</h3><br/>
                             <h4>$randomCode</h4>";
 
             $mail->addAddress($email);
