a383073f43af5a53e3b7f3b30e66273397fbdb9d
PrynsTag <201812311@fit.edu.ph>
2021-04-04 17:17:51 +0800

    change buttons text to HIRE and MESSAGE

--- gigs.php
+++ gigs.php
@@ -66,10 +66,10 @@
               <!-- Buttons -->
               <div class="grid grid-cols-2 px-5 pb-5 mt-5">
                 <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
-                  <a href="./update_card.php?card_id=<?php echo $userResult['gigs_id']; ?>" class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</a>
+                  <a href="./update_card.php?card_id=<?php echo $userResult['gigs_id']; ?>" class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">HIRE</a>
                 </div>
                 <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
-                  <a href="./card_delete.php?card_id=<?php echo $userResult['gigs_id']; ?>" class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</a>
+                  <a href="./card_delete.php?card_id=<?php echo $userResult['gigs_id']; ?>" class="delete-button h-10 py-2 px-4 rounded-lg tracking-wide">MESSAGE</a>
                 </div>
               </div>
             </div>
