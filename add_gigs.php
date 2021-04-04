212778cc1c31727c62e9b835290691b1dfbcb7af
Katherine Mariñas <55967594+Kathorsiii@users.noreply.github.com>
2021-04-04 18:08:39 +0800

    Update add_gigs.php

--- add_gigs.php
+++ add_gigs.php
@@ -9,14 +9,13 @@
 
   <section class="mx-auto">
     <div>
-      <h1 class="title-header font-bold">Add Gigs</h1>
+      <div class="text-5xl pt-3 text-center md:mb-12 md:pt-10 lg:pt-3 xl:pt-3 2xl:pt-3 font-black mb-12">Add Gigs</div>
     </div>
 
     <form action="./check_gigs.php" method="POST" enctype="multipart/form-data" id="addgigs">
       <div class="antialiased flex justify-center">
-        <div class="px-4 py-5 bg-white space-y-6 sm:p-6 shadow sm:rounded-md sm:overflow-hidden w-6/12">
-          <h3 class="text-lg font-medium leading-6 text-gray-900">What is your service...</h3>
-
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6 shadow sm:overflow-hidden rounded-lg w-5/6 lg:w-1/2 xl:w-1/2 2xl:w-1/2">
+          <div class="text-lg font-medium leading-6 text-gray-900 text-center pt-5">What is your service...</div>
           <div class="px-28 flex justify-end">
           </div>
           <!-- Input Fields  -->
@@ -28,9 +27,8 @@
               </label>
 
               <div class="relative margin">
-                <select name="service" id="service"
-                        class="w-full appearance-none h-10 pl-3 pr-12 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 outline-none cursor-pointer">
-                  <option value="">-Select Service-</option>
+                <select name="service" id="service" class="w-full appearance-none h-10 pl-3 pr-12 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 outline-none cursor-pointer">
+                  <option value="">Select Service</option>
                   <?php
                   $sql = "SELECT * FROM freelance_service";
                   $result = $conn->query($sql);
@@ -41,8 +39,7 @@
                 </select>
 
                 <div class="absolute inset-y-0 right-0 flex items-center pr-4 cursor-pointer">
-                  <img src="./assets/img/dropdown-icon.svg" alt="password-icon" class="h-6 w-6 hide"
-                       id="current-password-icon">
+                  <img src="./assets/img/dropdown-icon.svg" alt="password-icon" class="h-6 w-6 hide" id="current-password-icon">
                 </div>
               </div>
             </div>
@@ -54,10 +51,7 @@
               </label>
               <div class="relative text-gray-700">
                 <label>
-                  <textarea rows="20" cols="50" form="addgigs"
-                            class="w-full h-24 py-2 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                            name="description" id="description" placeholder="Enter your description..."
-                            required></textarea>
+                  <textarea rows="20" cols="50" form="addgigs" class="w-full h-24 py-2 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" name="description" id="description" placeholder="Enter your description..." required></textarea>
                 </label>
               </div>
               <!-- Banner -->
@@ -65,33 +59,28 @@
                 Banner Profile
               </label>
               <div class="border border-dashed border-gray-500 relative">
-                <input type="file" id="banner" name="imgToUpload" multiple
-                       class="cursor-pointer relative block opacity-0 p-20 z-50">
-                <div class="text-center p-10 absolute top-0 right-0 left-0 m-auto">
-                  <h4>
+                <input type="file" accept="image/*" id="banner" name="imgToUpload" multiple class="cursor-pointer relative block opacity-0 p-8 lg:p-12 xl:p-12 2xl:p-12 z-50">
+                <div class="text-center pt-4 lg:pt-6 xl:pt-6 2xl:pt-6 absolute top-0 right-0 left-0 m-auto">
+                  <div class="text-sm sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base">
                     Drop files anywhere to upload
-                    <br/>or
-                  </h4>
-                  <p class="">Select Files</p>
+                    <br/><div class="text-sm sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base">or</div>
+                  </div>
+                  <div class="text-sm sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base">Select Files</div>
                 </div>
               </div>
               <!-- Price -->
-              <label for="price" class="block text-xs text-gray-600 dark:text-gray-400 uppercase font-black">
+              <label for="price" class="block pt-1 text-xs text-gray-600 dark:text-gray-400 uppercase font-black">
                 Price (₱)
               </label>
               <div class="relative text-gray-700">
                 <label>
-                  <input onkeypress="return isNumberKey(event)"
-                         class="w-full h-10 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 appearance-none"
-                         type="text" name="price" id="price" required/>
+                  <input onkeypress="return isNumberKey(event)" class="w-full h-10 pl-3 pr-8 text-base rounded-md border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 appearance-none" type="text" name="price" id="price" required/>
                 </label>
               </div>
               <!-- CANCEL AND SAVE BUTTONS -->
               <div class="flex justify-center px-4 py-3 text-right sm:px-6 space-x-6">
-                <a href="./mygigs.php"
-                   class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-lg text-white bg-blue-change hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer">Cancel</a>
-                <input type="submit" value="Add" name="addGigs"
-                       class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-lg text-white bg-blue-save focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer"/>
+                <input type="submit" value="Add" name="addGigs" class="inline-flex justify-center py-3 px-7 border shadow-sm text-sm rounded-lg add-button cursor-pointer"/>
+                <a href="./mygigs.php" class="inline-flex justify-center py-3 px-7 border cancel-button text-sm rounded-lg cursor-pointer">Cancel</a>
               </div>
             </div>
           </div>
