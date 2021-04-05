7df93ac219ee8485c988fabff8693bf60aed31a1
Katherine Mariñas <55967594+Kathorsiii@users.noreply.github.com>
2021-04-05 10:24:56 +0800

    Update mygigs.php

--- mygigs.php
+++ mygigs.php
@@ -56,7 +56,7 @@
             $timeline_pic = $dbFilePath;
           }
         ?>
-          <div class="mx-auto justify-center w-full sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 p-4 text-center">
+          <div class="justify-center w-full sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4 text-center">
             <div class="card-gig block shadow-md hover:shadow-xl rounded-lg overflow-hidden">
               <div class="relative pb-48 overflow-hidden">
                 <img class="absolute inset-0 h-full w-full object-cover" src="<?= $timeline_pic ?>" alt="">
@@ -95,10 +95,10 @@
               <!-- Buttons -->
               <div class="grid grid-cols-2 px-5 pb-5 mt-5">
                 <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
-                  <a href="./update_card.php?card_id=<?php echo $userResult['gigs_id']; ?>" class="update-button h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</a>
-                </div>
-                <div class="col-span-1 w-100 flex flex-col justify-center pl-1">
                   <a href="./card_delete.php?card_id=<?php echo $userResult['gigs_id']; ?>" class="delete-button border h-10 py-2 px-4 rounded-lg tracking-wide">DELETE</a>
+                </div>
+                <div class="col-span-1 w-100 flex flex-col justify-center pr-1">
+                  <a href="./update_card.php?card_id=<?php echo $userResult['gigs_id']; ?>" class="update-button border h-10 py-2 px-4 rounded-lg tracking-wide">UPDATE</a>
                 </div>
               </div>
             </div>
