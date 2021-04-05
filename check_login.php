0f62edf470d197b8d66dc6c6d2da3390590c4abd
PrynsTag <201812311@fit.edu.ph>
2021-04-05 13:34:27 +0800

    refactored and added more checks on if else statements

--- check_login.php
+++ check_login.php
@@ -6,56 +6,46 @@ if (isset($_POST["login"], $conn) && $conn) {
 
     //Check input if empty
     if ((!empty($_POST['email'])) && (!empty($_POST['password']))) {
-
-        //Check Account return Boolean
+        // POST values
+        $user_email = $_POST["email"];
         $shaPass = sha1($_POST['password']);
         $origPass = $_POST['password'];
-        $ckAccountRows = checkAccount($conn, $_POST["email"], $shaPass);
 
-        if ($ckAccountRows !== 0 && $ckAccountRows !== null) {
-            $user_email = $_POST["email"];
-            $user_password = sha1($_POST["password"]);
-            $sql = "SELECT freelance_id, freelance_fName, freelance_lName, freelance_username, freelance_email, freelance_password, freelance_email_validation FROM freelance_info 
-                    WHERE freelance_email = '$user_email' AND freelance_password = '$user_password'";
-            $result = $conn->query($sql) or die($conn->error);
-            $userCount = $result->num_rows;
-            $userResult = $result->fetch_assoc();
+        $sql = "SELECT freelance_id, freelance_fName, freelance_lName, freelance_username, freelance_email, freelance_password, freelance_email_validation FROM freelance_info 
+                WHERE freelance_email = '$user_email'";
+        $result = $conn->query($sql) or die($conn->error);
+        $userCount = $result->num_rows;
+        $userResult = $result->fetch_array();
 
-            if ($userCount === 1) {
+        if ($userCount === 1 && $userResult["freelance_password"] === $shaPass) {
+            if ($userResult['freelance_email_validation'] === '1') {
 
-                if ($userResult['freelance_email_validation'] == 1) {
+                [$_SESSION['user_id'], $_SESSION['user_firstname'], $_SESSION['user_lastname'],
+                    $_SESSION['user_username'], $_SESSION['user_email'], $_SESSION['user_password'], $code] = $userResult;
 
-                    $_SESSION['user_id'] = $userResult['freelance_id'];
-                    $_SESSION['user_firstname'] = $userResult['freelance_fName'];
-                    $_SESSION['user_lastname'] = $userResult['freelance_lName'];
-                    $_SESSION['user_username'] = $userResult['freelance_username'];
-                    $_SESSION['user_email'] = $userResult['freelance_email'];
-                    $_SESSION['user_password'] = $userResult['freelance_password'];
-
-                    if (isset($_POST['rememberme'])) {
-                        setcookie("user_email", infoEncrypt($user_email), time() + 86400);
-                        setcookie("user_password", infoEncrypt($origPass), time() + 86400);
-                    } else {
-                        setcookie("user_email", "", time() - 86400);
-                        setcookie("user_password", "", time() - 86400);
-                    }
-
-                    echo "<script>alert('Login Successfully.')
-                    window.location.href='gigs.php';
-                    </script>";
+                if (isset($_POST['rememberme'])) {
+                    setcookie("user_email", infoEncrypt($user_email), time() + 86400);
+                    setcookie("user_password", infoEncrypt($origPass), time() + 86400);
                 } else {
-                    echo "<script>alert('Verify your account first. Please check your email.');
-                    window.location.href='index.php?login_status=not-verified';
-                    </script>";
+                    setcookie("user_email", "", time() - 86400);
+                    setcookie("user_password", "", time() - 86400);
                 }
+
+                echo "<script>alert('Login Successfully.')
+                window.location.href='gigs.php';
+                </script>";
             } else {
-                echo "<script>alert('User doesn\'t exists.')
-                window.location.href='login.php?account_status=not_exist';
+                echo "<script>alert('Verify your account first. Please check your email.');
+                window.location.href='index.php?login_status=not-verified';
                 </script>";
             }
-        } else {
-            echo "<script>alert('Username or Password is incorrect.')
+        } elseif ($userResult["freelance_email"] === $_POST["email"] || $userResult["freelance_password"] === $shaPass) {
+            echo "<script>alert('Email or Password is incorrect.')
             window.location.href='login.php?account_status=fail';
+            </script>";
+        } else {
+            echo "<script>alert('User doesn\'t exists.')
+            window.location.href='login.php?account_status=not_exist';
             </script>";
         }
     } else {
@@ -63,6 +53,10 @@ if (isset($_POST["login"], $conn) && $conn) {
         window.location.href='login.php?account_status=blank';
         </script>";
     }
+} elseif (isset($conn) && $conn) {
+    echo "<script>alert('Please submit your credentials properly.')
+    window.location.href='login.php?account_status=no_submit';
+    </script>";
 } else {
     trigger_error("Connection failed: " . $conn->connect_error);
 }
