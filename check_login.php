28b935dc165b67d8f35c40bf1c2bc0a94f96ba12
CWG.Ga <201812027@fit.edu.ph>
2021-04-12 15:37:22 +0800

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -8,8 +8,8 @@ if (isset($_POST["login"], $conn) && $conn) {
     if ((!empty($_POST['email'])) && (!empty($_POST['password']))) {
         // POST values
         $user_email = $_POST["email"];
-        $shaPass = sha1($_POST['password']);
         $origPass = $_POST['password'];
+        $shaPass = sha1($origPass);
 
         $sql = "SELECT freelance_id, freelance_fName, freelance_lName, freelance_username, freelance_email, freelance_password, freelance_email_validation FROM freelance_info 
                 WHERE freelance_email = '$user_email'";
@@ -17,32 +17,35 @@ if (isset($_POST["login"], $conn) && $conn) {
         $userCount = $result->num_rows;
         $userResult = $result->fetch_array();
 
-        if ($userCount === 1 && $userResult["freelance_password"] === $shaPass) {
+        if ($userCount === 1) {
+
             if ($userResult['freelance_email_validation'] === '1') {
 
-                [$_SESSION['user_id'], $_SESSION['user_firstname'], $_SESSION['user_lastname'],
-                    $_SESSION['user_username'], $_SESSION['user_email'], $code] = $userResult;
+                if ($userResult['freelance_password'] === $shaPass) {
+                    [
+                        $_SESSION['user_id'], $_SESSION['user_firstname'], $_SESSION['user_lastname'],
+                        $_SESSION['user_username'], $_SESSION['user_email'], $code
+                    ] = $userResult;
 
-                if (isset($_POST['rememberme'])) {
-                    setcookie("user_email", infoEncrypt($user_email), time() + 86400);
-                    setcookie("user_password", infoEncrypt($origPass), time() + 86400);
+                    if (isset($_POST['rememberme'])) {
+                        setcookie("user_email", infoEncrypt($user_email), time() + 86400);
+                        setcookie("user_password", infoEncrypt($origPass), time() + 86400);
+                    } else {
+                        setcookie("user_email", "", time() - 86400);
+                        setcookie("user_password", "", time() - 86400);
+                    }
+
+                    header("location: gigs.php");
                 } else {
-                    setcookie("user_email", "", time() - 86400);
-                    setcookie("user_password", "", time() - 86400);
+                    echo "<script>alert('Email or Password is incorrect. Please try again.')
+                        window.location.href='login.php?account_status=fail';
+                        </script>";
                 }
-
-                echo "<script>alert('Login Successfully.')
-                window.location.href='gigs.php';
-                </script>";
             } else {
                 echo "<script>alert('Verify your account first. Please check your email.');
                 window.location.href='index.php?login_status=not-verified';
                 </script>";
             }
-        } elseif ($userResult["freelance_email"] === $_POST["email"] || $userResult["freelance_password"] === $shaPass) {
-            echo "<script>alert('Email or Password is incorrect.')
-            window.location.href='login.php?account_status=fail';
-            </script>";
         } else {
             echo "<script>alert('User doesn\'t exists.')
             window.location.href='login.php?account_status=not_exist';
